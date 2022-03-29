be8cdb4e3b540b0da58f76408bc888c8010c0ecb
leonela22 <rhungwe@icloud.com>
2022-03-29 22:14:12 +0100

    Homework 4

---
+++ Homework 4.py
@@ -0,0 +1,61 @@
+# Question 1
+# Complete definitions for key Git & GitHub terminology
+# GIT WORKFLOW FUNDAMENTALS
+
+print("Hello world")
+
+# Working Directory
+# Staging Area
+# Local Repo (head)
+# Remote repo (master)
+
+
+# WORKING DIRECTORY STATES:
+# Staged
+# Modified
+# Committed
+
+
+
+# GIT COMMANDS:
+# Git add
+# Git commit
+# Git push
+# Git fetch
+# Git merge
+# Git pull
+
+
+
+
+# TASK 2 (Exception Handling)
+#
+# Question 1
+# Simple ATM program
+# Using exception handling code blocks such as try/ except / else / finally, write a program
+# that simulates an ATM machine to withdraw money.
+# (NB: the more code blocks the better, but try to use at least two key words e.g. try/except)
+#
+# Tasks:
+# 1.Prompt user for a pin code
+# 2.If the pin code is correct then proceed to the next step, otherwise ask a user to type
+# in a password again. You can give a user a maximum of 3 attempts and then exit a program.
+# 3.Set account balance to 100.
+# 4.Now we need to simulate cash withdrawal
+# 5.Accept the withdrawal amount
+# 6.Subtract the amount from the account balance and display the remaining balance
+# (NOTE! The balance cannot be negative!)
+# 7.However, when a user asks to withdraw more money than they have on their account
+# then you need to raise an error an exit the program.
+
+
+
+
+
+# TASK 3 (Testing)
+#
+# Question 1
+# Use the Simple ATM program to write unit tests for your functions.
+# You are allowed to re-factor your function to untangle some logic into smaller
+# blocks of code to make it easier to write tests.
+# Try to write at least 5 unit tests in total covering various cases.
